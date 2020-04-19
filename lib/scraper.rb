6e8c1e0c1bfef1750f530ef3aa6cb513dccc9592
Rwanda S <rwanda.ace@gmail.com>
2020-04-19 15:13:43 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -39,9 +39,10 @@ class Heros
              all_role_types = all_role_types.collect {|x| x}.uniq
               all_affiliation_types = all_affiliation_types.collect {|x| x}.uniq
   end
-  all_hero_names.collect {|x| 
-  all_heroes[x]["role"] = "Tank"
+  all_hero_names.collect {|x|   if all_heroes[x]["role"] == "Tank"
     x
+  end
+  }
   }
   
   
